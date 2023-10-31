81d68fda3809d97d7b818117aef166f3d78a23e3
Growingbestjanani1521 <janananimani1590@gmail.com>
2023-10-31 13:54:19 +0530

    task completed

---
+++ script.js
@@ -0,0 +1,76 @@
+//class Person => names,age,gender,salary
+/*class Person{
+    constructor(names,age,gender,salary=50){
+        this.names = names;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+    }
+}
+const  person1 = new Person("janani",33,"female");
+const  person2 = new Person("janani",33,"female",60);
+console.log(person1.names);
+console.log(person1.age);
+console.log(person1.gender);
+console.log(person1.salary);
+console.log(person2.salary);*/
+
+// CIRCLE UML DIAGRAM
+
+/*class Circle{
+    constructor(radius,color){
+        this.radius = radius;
+        this.colour = color;
+  }
+get.radiusCircle(){
+    return this.radius;
+},
+set. radiusCircle(radius){
+    this._radius = radius;
+},
+get. colorCircle(){
+    return this.color;
+},
+set. colorCircle(color){
+    this._color = color;
+},
+get. areaCircle(){
+    return Math.PI this.area;
+},
+get. tostring(){
+    return `"Circle [radius = $ {this.radius}, color = $ {this.color}]"`;
+},
+get. cirucumferenceCircle(){
+    return 2* Math.PI * this.radius;
+}
+
+}
+
+var obj1 = new Circle(1.0,"red");
+console.log(obj1.radiusCircle);
+obj1.radiusCircle = 2.2;
+console.log(obj1.colorCricle);
+console.log(obj1.areaCricle);
+console.log(obj1.circumferenceCricle);
+console.log(obj1.radiusCricle);*/
+
+/*class Movie{
+    constructor(rating,review)
+    this.rating = "rating";
+    this.review = "review";
+}
+const Movie1 = new Movie(5,good);
+console.log(movie1.rating);*/
+
+/*class Uber(){
+    constructor(price per miles, price per minutes);{
+    this.price per miles = "price per miles";
+    this.price per minutes = "price per minutes"
+    }
+}
+const Uber1 = new Uber(50);
+console.log(Uber1.pricepermiles);*/
+
+
+
+
